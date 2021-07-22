9e970ffd4e4cd2d62b57d68859f69b6f6f90f629
magesh1 <mageshcruz@gmail.com>
2021-07-22 13:23:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const date = require(__dirname + "/date.js");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+const items = [];
+const workItems = [];
+
+app.get("/", function (req, res) {
+
+    const day = date.getDate();
+    res.render("List", {
+        listTitle: day,
+        newListItems: items
+    });
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    })
+});
+
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+
+app.listen(3000, () => console.log('server listening 3000'))
